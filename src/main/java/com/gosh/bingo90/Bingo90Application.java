b81f763ee74a304044c218e896482915cfe7c88f
JussV <joana.veljanoska@gmail.com>
2020-12-22 22:25:54 +0100

    Initial commit

---
+++ src/main/java/com/gosh/bingo90/Bingo90Application.java
@@ -0,0 +1,13 @@
+package com.gosh.bingo90;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bingo90Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bingo90Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gosh/bingo90/Bingo90ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosh.bingo90;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bingo90ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
