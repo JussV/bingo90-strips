a58963ffb4bad0e8765f1050a7c388e11628fd3c
JussV <joana.veljanoska@gmail.com>
2020-12-26 00:51:33 +0100

    add unit test to validate column ranges

--- src/test/java/com/gosh/bingo90/service/BingoStripGeneratorTest.java
+++ src/test/java/com/gosh/bingo90/service/BingoStripGeneratorTest.java
@@ -13,20 +13,39 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BingoStripGeneratorTest {
 
     private static BingoStrip bingoStrip;
+    private static Map<Integer, List<int[]>> tickets;
     private static final int EXPECTED_TOTAL_NUMBERS = 90;
     private static final int EXPECTED_TOTAL_NUMBERS_PER_ROW = 5;
     private static final int EXPECTED_TOTAL_NUMBERS_PER_TICKET = 15;
     private static final int EXPECTED_NUMBER_OF_TICKETS = 6;
     private static final int EXPECTED_ROWS_PER_TICKET = 3;
+    private static final int MAX_NUMBERS_PER_TICKET_COLUMN = 3;
+    private static final int MIN_NUMBERS_PER_TICKET_COLUMN = 1;
+    private static final Map<Integer, Integer> EXPECTED_COLUMN_NUMBERS_UP_TO = Map.of(
+            0,10,
+            1,20,
+            2,30,
+            3,40,
+            4,50,
+            5,60,
+            6,70,
+            7,80,
+            8,91 // 90 is inclusive in this list
+    );
+
 
     @BeforeAll
     public static void generateBingoStrip() {
         BingoStripGenerator bingoStripGenerator = new BingoStripGenerator(Instant.now().toEpochMilli());
         bingoStrip = bingoStripGenerator.generateStrip();
+        AtomicInteger index = new AtomicInteger(0);
+        tickets = Arrays.stream(bingoStrip.getStrip())
+                .collect(Collectors.groupingBy(row -> List.of(0,1,2,3,4,5).get(index.getAndIncrement() / 3)));
     }
 
     @Test
@@ -48,29 +67,19 @@ class BingoStripGeneratorTest {
                 .map(arr -> Arrays.stream(arr).filter(x -> x > 0).boxed().collect(Collectors.toList()))
                 .collect(Collectors.toList());
 
-        rowNumbers.forEach(row -> {
-            assertEquals(EXPECTED_TOTAL_NUMBERS_PER_ROW, row.size());
-        });
+        rowNumbers.forEach(row -> assertEquals(EXPECTED_TOTAL_NUMBERS_PER_ROW, row.size()));
     }
 
     @Test
     void stripConsistsOf6TicketsWith3Rows() {
-        AtomicInteger index = new AtomicInteger(0);
-        Map<Integer, List<int[]>> tickets = Arrays.stream(bingoStrip.getStrip())
-            .collect(Collectors.groupingBy(row -> List.of(0,1,2,3,4,5).get(index.getAndIncrement() / 3)));
         assertEquals(EXPECTED_NUMBER_OF_TICKETS, tickets.entrySet().size());
-        tickets.forEach((ticketId, ticketRows) -> {
-            assertEquals(EXPECTED_ROWS_PER_TICKET, ticketRows.size());
-        });
+        tickets.forEach((ticketId, ticketRows) -> assertEquals(EXPECTED_ROWS_PER_TICKET, ticketRows.size()));
     }
 
     @Test
     void ticketConsistOf15Numbers() {
-        AtomicInteger index = new AtomicInteger(0);
-        Map<Integer, List<int[]>> tickets = Arrays.stream(bingoStrip.getStrip())
-            .collect(Collectors.groupingBy(row -> List.of(0,1,2,3,4,5).get(index.getAndIncrement() / 3)));
-        tickets.forEach((key, value) -> {
-            List<Integer> ticketNumbers = value
+        tickets.forEach((ticketId, ticket) -> {
+            List<Integer> ticketNumbers = ticket
                 .stream()
                 .flatMapToInt(Arrays::stream)
                 .boxed()
@@ -80,6 +89,17 @@ class BingoStripGeneratorTest {
         });
     }
 
-
+    @Test
+    void ticketColumnContainsMin1Max3AndIsWithinColumnRange() {
+        tickets.forEach((ticketId, ticket) -> {
+            IntStream.range(0, 9).boxed().forEach(columnId -> {
+                List<Integer> columnNumbers = ticket.stream().map(row -> row[columnId]).collect(Collectors.toList());
+                long totalNumbersPerColumn = columnNumbers.stream().filter(n -> n > 0).count();
+                assertTrue(totalNumbersPerColumn <= MAX_NUMBERS_PER_TICKET_COLUMN);
+                assertTrue(totalNumbersPerColumn >= MIN_NUMBERS_PER_TICKET_COLUMN);
+                columnNumbers.forEach(colNum -> assertTrue(colNum < EXPECTED_COLUMN_NUMBERS_UP_TO.get(columnId)));
+            });
+        });
+    }
 
 }
